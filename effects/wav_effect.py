572ea2632ee07fb81b8ed811e4fb3e10964014f2
Kyle Corry <kylecorry31@gmail.com>
2021-10-30 19:44:40 -0400

    Add wav effect

---
+++ effects/wav_effect.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pyaudio
+import wave
+import time
+import threading
+
+class _WavSong:
+    def __init__(self, file, lights, scale, interval):
+        self.p = pyaudio.PyAudio()
+        self.file = file
+        self.scale = scale
+        self.lights = lights
+        self.interval = interval
+        self.stream = None
+        self.last = time.time()
+
+    def __play_note(self, lights, reading):
+        for light in lights:
+            light.on(reading, self.interval)
+
+    def __play_callback(self, in_data, frame_count, time_info, status):
+        d = self.wf.readframes(frame_count)
+        data = np.frombuffer(d, dtype=np.int16)
+        reading = (np.max(np.abs(data)) / 32767.0) * 255 * self.scale
+        if time.time() - self.last > self.interval:
+            self.last = time.time()
+            thread = threading.Thread(target=self.__play_note, args=(self.lights, reading))
+            thread.daemon = True
+            thread.start()
+        return (data, pyaudio.paContinue)
+
+    def play(self):
+        self.wf = wave.open(self.file, 'rb')
+        self.stream = self.p.open(format=self.p.get_format_from_width(self.wf.getsampwidth()),
+                             channels=self.wf.getnchannels(), rate=self.wf.getframerate(),
+                             output=True, stream_callback=lambda in_data, frame_count, time_info, status: self.__play_callback(in_data, frame_count, time_info, status))
+        self.stream.start_stream()
+        while self.stream.is_active():
+            time.sleep(0.1)
+        self.stop()
+
+    def stop(self):
+        if self.stream is not None:
+            self.stream.stop_stream()
+            self.stream.close()
+            self.stream = None
+
+class WavEffect:
+
+    def __init__(self, file, scale, resolution):
+        self.file = file
+        self.scale = scale
+        self.resolution = resolution
+
+    def run(self, lights):
+        song = _WavSong(self.file, lights, self.scale, self.resolution)
+        song.play()
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from utils.lights import get_lights
 from effects.candle_effect import CandleEffect
 from effects.phasma_hunt_effect import PhasmaHuntEffect
+from effects.wav_effect import WavEffect
 
 lights = get_lights(2)
 
 try:
     # PhasmaHuntEffect(250).run([left, right])
     CandleEffect(250, 45).run(lights)
+    # WavEffect("wav/4.wav", 2, 0.01).run(lights)
 except:
     for light in lights:
         light.on(255)
